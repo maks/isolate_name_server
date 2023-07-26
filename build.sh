2c9abf4aa0343447f53e97ed1a487b549d0af79f
Maksim Lin <maks@manichord.com>
2023-07-26 20:22:19 +1000

    use Dart SDK env var from Dart Action

--- build.sh
+++ build.sh
@@ -3,14 +3,9 @@
 # build the minimal basic native code, HARDCODED to paths on Ubuntu 22.04
 cd native || exit
 rm *.o *.so
-# gcc -c -Wall -Werror -fpic return_port.cc /usr/lib/dart/include/dart_api_dl.c
-# gcc -shared -o libreturnport.so return_port.o dart_api_dl.o
 
-echo Dart SDK location:
-which dart
+Run echo "Dart SDK installed in $DART_HOME"
+ls $DART_HOME/include/dart_api_dl.c
 
-echo check expected path:
-ls -R /opt/hostedtoolcache/dart/
-
-gcc -c -Wall -Werror -fpic isolate_name_server.c /usr/lib/dart/include/dart_api_dl.c
+gcc -c -Wall -Werror -fpic isolate_name_server.c $DART_HOME/include/dart_api_dl.c
 gcc -shared -o libnameserver.so isolate_name_server.o dart_api_dl.o
