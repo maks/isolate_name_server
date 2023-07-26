db9606af90a03d8ba5eb90ec0f65cf5c0aef5e63
Maksim Lin <maks@manichord.com>
2023-07-26 20:09:38 +1000

    debug dart sdk on CI

--- build.sh
+++ build.sh
@@ -6,5 +6,11 @@ rm *.o *.so
 # gcc -c -Wall -Werror -fpic return_port.cc /usr/lib/dart/include/dart_api_dl.c
 # gcc -shared -o libreturnport.so return_port.o dart_api_dl.o
 
+echo Dart SDK location:
+which dart
+
+echo check expected path:
+ls /usr/lib/dart
+
 gcc -c -Wall -Werror -fpic isolate_name_server.c /usr/lib/dart/include/dart_api_dl.c
 gcc -shared -o libnameserver.so isolate_name_server.o dart_api_dl.o
