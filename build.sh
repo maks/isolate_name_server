1f7acb358e3f872790b8392e12c72e98461c2d27
Maksim Lin <maks@manichord.com>
2023-07-26 20:23:56 +1000

    fix typo

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 cd native || exit
 rm *.o *.so
 
-Run echo "Dart SDK installed in $DART_HOME"
-ls $DART_HOME/include/dart_api_dl.c
+echo "Dart SDK installed in $DART_HOME"
+ls -l $DART_HOME/include/dart_api_dl.c
 
 gcc -c -Wall -Werror -fpic isolate_name_server.c $DART_HOME/include/dart_api_dl.c
 gcc -shared -o libnameserver.so isolate_name_server.o dart_api_dl.o
