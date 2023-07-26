160a77e196ea84e520507b0241a9dbd8c24c5d01
Maksim Lin <maks@manichord.com>
2023-07-26 18:41:00 +1000

    add locking in native code, tests todo

--- native/isolate_name_server.c
+++ native/isolate_name_server.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <pthread.h>
+
 #include "hashmap.h"
 
 #include "isolate_name_server.h"
 
 static struct hashmap_s hashmap;
+pthread_mutex_t lock;
 
 int initNameServer() {
     const unsigned initial_size = 10;    
@@ -11,7 +14,9 @@ int initNameServer() {
 }
 
 Dart_Handle lookupPortByName(const char* name) {
+    pthread_mutex_lock(&lock);
     Dart_Port* port = hashmap_get(&hashmap, name, strlen(name));
+    pthread_mutex_unlock(&lock);
     if (port != NULL) {
         return Dart_NewSendPort(*port);
     } else {        
@@ -20,15 +25,21 @@ Dart_Handle lookupPortByName(const char* name) {
 }
 
 int registerPortWithName(Dart_Port port, const char*  name) {
+    pthread_mutex_lock(&lock);
     void* existing = hashmap_get(&hashmap, name, strlen(name));
     if (existing != NULL) {
+        pthread_mutex_unlock(&lock);
         return 1; 
     }
     Dart_Port* val = malloc(sizeof(int));
     *val = port;
+    pthread_mutex_unlock(&lock);
     return hashmap_put(&hashmap, name, strlen(name), val);
 }
 
 int removePortNameMapping(const char* name) {
-    return hashmap_remove(&hashmap, name, strlen(name));
+    pthread_mutex_lock(&lock);
+    int err = hashmap_remove(&hashmap, name, strlen(name));
+    pthread_mutex_unlock(&lock);
+    return err;
 }
